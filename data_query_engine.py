59289753a878c430b12755181c736088914e300b
Quyen Huynh <samhuynh@Sams-MacBook-Pro-94.local>
2023-12-12 14:43:47 -0500

    analysis

--- data_query_engine.py
+++ data_query_engine.py
@@ -2,6 +2,8 @@ import pandas as pd
 import sqlite3
 import numpy as np
 import networkx as nx
+import timeit
+import matplotlib.pyplot as plt
 
 
 class Engine:
@@ -48,7 +50,7 @@ class Engine:
 
     def community_search(self, conditions):
         category_condition = conditions[0]
-        query = f"SELECT * FROM my_table WHERE {category_condition}"
+        query = f"SELECT * FROM my_table WHERE my_table.category = 'Comedy'"
         self.cursor.execute(query)
         community_results = self.cursor.fetchall()
         return community_results
@@ -85,27 +87,43 @@ class Engine:
 
         return aggregated_graph
 
+    def performance_comparison(self):
+        # Define test conditions
+        entity_conditions = ["category = 'Comedy'", "views > 1000"]
+        ranged_category, min_duration, max_duration = 'Comedy', 100, 500
+        community_conditions = "category = 'Comedy'"
 
+        # Measure the execution time for entity_search
+        time_entity_search = timeit.timeit(lambda: self.entity_search(entity_conditions), number=100)
+        print(f"Entity Search Execution Time: {time_entity_search} seconds")
 
+        # Measure the execution time for ranged_query
+        time_ranged_query = timeit.timeit(lambda: self.ranged_query(ranged_category, min_duration, max_duration), number=100)
+        print(f"Ranged Query Execution Time: {time_ranged_query} seconds")
 
+        # Measure the execution time for community_search
+        time_community_search = timeit.timeit(lambda: self.community_search(community_conditions), number=100)
+        print(f"Community Search Execution Time: {time_community_search} seconds")
 
+        # Plot the performance comparison
+        labels = ['Entity Search', 'Ranged Query', 'Community Search']
+        execution_times = [time_entity_search, time_ranged_query, time_community_search]
 
+        plt.bar(labels, execution_times)
+        plt.xlabel('Search Methods')
+        plt.ylabel('Execution Time (seconds)')
+        plt.title('Performance Comparison of Search Methods')
+        plt.show()
 
-
-
-
-
-
-
+# Create an instance of the Engine class
 engine = Engine(db="youtube_data.db", df=pd.read_csv("Data/0.txt", sep="\t", header=None))
-#engine.output_ranged_query('Comedy', 60, 120)
 
-engine.output_entity_search("category = 'Comedy'")
-
-
+# Perform the performance comparison for the 'Comedy' category
+engine.performance_comparison()
 
 
-engine.aggregate_nodes_by_type()
+
+
 
 
 
